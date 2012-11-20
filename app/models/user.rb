45150da3e185ede34407b54e90ba48b0e79aa0f4
inaki ibarra <inaki.mateo.ibarra@gmail.com>
2012-11-20 16:17:20 +0800

    deleted some links, add PROFILE PIC field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  has_attached_file :pic, :styles => 
+           { :medium => "100x100>", :thumb => "50x50>" }
+   #has_attached_file :attach
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :fname, :lname, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :fname, :lname, :email, :password, :password_confirmation, :remember_me, :pic
   # attr_accessible :title, :body
 end
