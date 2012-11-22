d9b26bffc36319ff299244992fd5c869240188bb
alenteria <arnel.lenteria@gmail.com>
2012-11-22 14:42:04 +0800

    comment form in view/post/show updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :fname, :lname, :email, :password, :password_confirmation, :remember_me, :pic
+  attr_accessible :id, :fname, :lname, :email, :password, :password_confirmation, :remember_me, :pic
   # attr_accessible :title, :body
   
   
