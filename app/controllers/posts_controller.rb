ecd87b01b7257412be2b59e644dce70ea2f05587
alenteria <arnel.lenteria@gmail.com>
2012-11-22 10:22:59 +0800

    post method update, destroy complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
       #random number generator
-      key=SecureRandom.hex(5)
+      key=SecureRandom.hex(5).upcase
       nkey="TCK:#{key}"
       params[:post][:namekey] = nkey
     
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
       
       if @post.save
         post_id= @post.id
-        puts assigned_usrs.size
+        
         for n in 1...assigned_usrs.size do
            assigned = Assignment.new(:post_id => post_id, :user_id => assigned_usrs[n])
            assigned.save
@@ -80,10 +80,29 @@ class PostsController < ApplicationController
   # PUT /posts/1
   # PUT /posts/1.json
   def update
+    
+    assigned_usrs=params[:post][:user_id]
+    params[:post][:user_id] = current_user.id
+    
+    @assignment= Assignment.find_by_post_id(params[:id])
     @post = Post.find(params[:id])
-
+    
     respond_to do |format|
       if @post.update_attributes(params[:post])
+         post_id= @post.id
+         
+         if assigned_usrs.size<=1
+            #do nothing
+          elsif
+            Assignment.delete_all(:post_id => params[:id])
+            for n in 1...assigned_usrs.size do
+             assigned = Assignment.new(:post_id => post_id, :user_id => assigned_usrs[n])
+             assigned.save
+            end
+          end
+        
+        
+       
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
@@ -97,8 +116,13 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
+  # @assignments= Assignment.find_by_post_id(params[:id])
+    
+    Assignment.delete_all(:post_id => params[:id])
+    
     @post.destroy
-
+   # @assignments.destroy
+    
     respond_to do |format|
       format.html { redirect_to posts_url }
       format.json { head :no_content }
